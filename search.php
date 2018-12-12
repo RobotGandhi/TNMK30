f7ac33d94d452dd719d67ce890063e4f4bb0ea28
RobotGandhi <99viktor.vc@gmail.com>
2018-12-12 10:57:35 +0100

    Update search.php

--- search.php
+++ search.php
@@ -31,17 +31,25 @@
 		</div>
         
         <div class= "content">
+
             <?php
             $connection = mysqli_connect("mysql.itn.liu.se","lego","","lego");
             if (!$connection){
                 die("No connection to the lego database could be established.");
             }
-            $result = mysqli_query($connection, "SELECT PartID FROM parts LIMIT 100");
+            $result = mysqli_query($connection, "SELECT DISTINCT PartID, partname FROM parts LIMIT 100");
             print("<table>\n<tr>");
             while($fieldinfo = mysqli_fetch_field($result)) {
 	           print("<th>". $fieldinfo->name . "</th>");
             }
-            print("</tr>\n</table>")
+            print("</tr>\n");
+            while($row = mysqli_fetch_row($result)){
+                print("<tr>");
+                for($i=0,$i<mysqli_num_fields($result),$i++){
+                    print("<td>".$row[$i]."</td>");
+                }
+                print("</tr>");
+            }
             ?>
         </div>
         
