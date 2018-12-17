350b508f65496db83207805313ff935f35166308
Almfa <45815449+LEGOpr@users.noreply.github.com>
2018-12-17 15:35:20 +0100

    Add files via upload

---
+++ searchresult.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+
+<html>
+
+    
+<head>
+    
+     <meta charset="UTF-8"/>
+    <link rel="stylesheet" href="proj.css" type="text/css"/>
+	<title>Search result</title>
+    
+</head>
+    
+<body background="lego-duplo-2304-lego-duplo-large-green-building-plate-2.jpg">
+        
+ <div class="wrapper">
+        
+     
+    
+        <div class ="header">
+       
+        <h1>This is the header</h1>
+		
+		
+		
+        </div>
+		
+         <?php include("menu.txt");?>
+		 
+		 <?php include("searchform.txt");?>
+		
+      <div class= "content">
+	  <h3> Result </h3> 
+<table>
+					<tr>
+						<th>Namn</th>
+						<th>namn:</th>
+						<th>Namn</th>
+						<th>namn:</th>
+					</tr>
+					<tr>
+						<td>test</td>
+						<td>test</td>
+						<td>ja</td>
+						<td>ja</td>
+					</tr>
+					
+				</table>
+        
+         
+            <!-- content content div closing tag  -->
+        </div>
+		
+		<div class= "content">
+		<h3> related parts </h3>
+		</div>
+    
+    
+
+    <div class = "footer">
+        <p>This is the footer</p>
+        
+        <!-- Footer content div closing tag  -->
+    </div>
+            
+<!-- wrapper content div closing tag  -->
+ </div>
+    
+</body>
+
+
+</html>
