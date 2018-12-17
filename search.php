396be15e5dd76dd034dc3c991bc87628ecdd967d
RobotGandhi <99viktor.vc@gmail.com>
2018-12-17 15:30:36 +0100

    Add files via upload

--- search.php
+++ search.php
@@ -17,7 +17,7 @@
 		<div class="search">
 		<form action="search.php" method="get">
 		<table>
-		<tr><td> <input type="text" name="searchkey"</td></tr>
+		<tr><td> <input type="text" name="searchkey"></td></tr>
 		</table>
 		</form>
 		</div>
@@ -25,14 +25,53 @@
 		<!-- Sökresultat efter string management -->
 		<div class="stringresult">
 		<?php
+		$x_amount = 0;
+		$number_amount = 1;
 		$searchkey = $_GET['searchkey'];
 		trim($searchkey);
 		$searchkeyarray = str_split($searchkey);
-		foreach($searchkeyarray as $char)
+		$newsearchkey = "";
+		for($i = 0; $i<count($searchkeyarray); $i++)
 		{
-			echo (" " .$char. " ");
+			if(is_numeric($searchkeyarray[$i]))
+			{
+				for($j = 0; $j < 6 && $j + $i < count($searchkeyarray); $j++)
+				{
+					//print(var_dump($searchkeyarray[$i + $j] == 'x'));
+					if($searchkeyarray[$i + $j] == 'x')
+					{
+						if ($j == 0) {
+							//print($searchkeyarray[$i + $j] . " " . var_dump($newsearchkey) . "\n");
+							break;
+						}
+						else{
+							$newsearchkey .= " x";
+							//print($searchkeyarray[$i + $j] . " " . var_dump($newsearchkey) . "\n");
+						}
+					}
+					
+					else if(is_numeric($searchkeyarray[$i + $j]))
+					{
+						if ($j == 0) {
+							$newsearchkey = substr($searchkey, 0, $i + 1);
+							//print(var_dump(substr($searchkey, 0, $i + 1)));
+						}
+						else{
+							$newsearchkey .= " " . $searchkeyarray[$i + $j];
+							//print($searchkeyarray[$i + $j] . " " . var_dump($newsearchkey) . "\n");
+						}
+					}
+					
+					else if($searchkeyarray[$i + $j] != " "){
+						$newsearchkey .= substr($searchkey, $i + $j - 1);
+							//print($searchkeyarray[$i + $j] . " " . var_dump($newsearchkey) . "\n");
+						$i =  $i + $j;
+						break;
+					}
+				}
+			}
 		}
-		print(" ".$searchkey." ");
+		print("\n\n\n".$newsearchkey." ");
 		?>
 		</div>
         
@@ -43,7 +82,7 @@
             if (!$connection){
                 die("No connection to the lego database could be established.");
             }
-            $result = mysqli_query($connection, "SELECT DISTINCT PartID, partname FROM parts WHERE partname LIKE '%$searchkey%' ORDER BY PartID DESC LIMIT 100");
+            $result = mysqli_query($connection, "SELECT DISTINCT * FROM parts WHERE partname LIKE '$searchkey%' ORDER BY length(CatID), CatID, partname ASC LIMIT 300");
             print("<table>\n<tr>");
             while($fieldinfo = mysqli_fetch_field($result)) {
 	           print("<th>".$fieldinfo->name."</th>");
@@ -54,8 +93,9 @@
                 for($i=0;$i<mysqli_num_fields($result);$i++){
                     print("<td>".$row[$i]."</td>");
                 }
-                print("</tr>");
+                print("</tr>\n");
             }
+			print("</table>");
             ?>
         </div>
         
