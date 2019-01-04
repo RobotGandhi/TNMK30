eb0c35a57f664456cefe072397382051bade09bc
Rubenbromee <45816070+Rubenbromee@users.noreply.github.com>
2019-01-04 14:52:32 +0100

    Add files via upload
    La till resultatssida för specifik färg. Resultatsbläddring behövs fortfarande.

--- search.php
+++ search.php
@@ -12,63 +12,62 @@
         <div class ="header">
             <h1>This is the header</h1>
         </div>
-		
-		<!-- Sökruta -->
-		<div class="search">
-		<form action="search.php" method="get">
-		<table>
-		<tr><td> <input type="text" name="searchkey"></td></tr>
-		</table>
-		</form>
-		</div>
-		
-		<!-- Sökresultat efter string management -->
-		<div class="stringresult">
-		<?php
-		$searchkey = $_GET['searchkey'];
-		trim($searchkey);
-		$searchkeyarray = str_split($searchkey);
-		for($i = 0; $i < count($searchkeyarray); $i++) {
-			if ($searchkeyarray[$i] == 'x'){
-				if (is_numeric($searchkeyarray[$i + 1])){
-					array_splice($searchkeyarray, $i + 1, 0, ' ');
-				}
-				if (is_numeric($searchkeyarray[$i - 1])){
-					array_splice($searchkeyarray, $i, 0, ' ');
-					$i++;
-				}
-			}
-		}
-		$searchkey = "";
-		for($i = 0; $i < count($searchkeyarray); $i++){
-			$searchkey .= $searchkeyarray[$i];
-		}
-		?>
-		</div>
+        
+        <!-- Sökruta -->
+        <div class="search">
+        <form action="search.php" method="get">
+        <table>
+        <tr><td> <input type="text" name="searchkey"></td></tr>
+        </table>
+        </form>
+        </div>
+        
+        <!-- Sökresultat efter string management -->
+        <div class="stringresult">
+        <?php
+$searchkey = $_GET['searchkey'];
+trim($searchkey);
+$searchkeyarray = str_split($searchkey);
+for ($i = 0; $i < count($searchkeyarray); $i++) {
+    if ($searchkeyarray[$i] == 'x') {
+        if (is_numeric($searchkeyarray[$i + 1])) {
+            array_splice($searchkeyarray, $i + 1, 0, ' ');
+        }
+        if (is_numeric($searchkeyarray[$i - 1])) {
+            array_splice($searchkeyarray, $i, 0, ' ');
+            $i++;
+        }
+    }
+}
+$searchkey = "";
+for ($i = 0; $i < count($searchkeyarray); $i++) {
+    $searchkey .= $searchkeyarray[$i];
+}
+?>
+       </div>
         
         <div class="content">
 
             <?php
-            $connection = mysqli_connect("mysql.itn.liu.se","lego","","lego");
-            if (!$connection){
-                die("No connection to the lego database could be established.");
-            }
-            $result = mysqli_query($connection, "SELECT DISTINCT * FROM parts WHERE partname LIKE '%$searchkey%' ORDER BY length(CatID), CatID, partname ASC LIMIT 100");
-            print("<table>\n<tr>");
-	        print("<th>PartID</th> <th>Partname</th>");
-            print("</tr>\n");
-            while($row = mysqli_fetch_array($result))
-			{
-            $PartID = $row['PartID'];
-			$Partname = $row['Partname'];
-			print("<tr>");
-			print("<td>$PartID</td>");
-			print("<td><a href=\"searchresult.php?part=".$PartID."\">$Partname</a></td>");
-			print("</tr>");
-			}
-			print("</table>");
-            ?>
-        </div>
+$connection = mysqli_connect("mysql.itn.liu.se", "lego", "", "lego");
+if (!$connection) {
+    die("No connection to the lego database could be established.");
+}
+$result = mysqli_query($connection, "SELECT DISTINCT * FROM parts WHERE partname LIKE '%$searchkey%' OR PartID LIKE '%$searchkey%' ORDER BY length(CatID), CatID, partname ASC LIMIT 100");
+print("<table>\n<tr>");
+print("<th>PartID</th> <th>Partname</th>");
+print("</tr>\n");
+while ($row = mysqli_fetch_array($result)) {
+    $PartID   = $row['PartID'];
+    $Partname = $row['Partname'];
+    print("<tr>");
+    print("<td>$PartID</td>");
+    print("<td><a href=\"searchresult.php?part=" . $PartID . "\">$Partname</a></td>");
+    print("</tr>");
+}
+print("</table>");
+?>
+       </div>
         
         <div class="footer">
             <p>This is the footer</p>
--- searchresult.php
+++ searchresult.php
@@ -1,141 +1,113 @@
 <!DOCTYPE html>
 
 <html>
-
-    
 <head>
-    
-     <meta charset="UTF-8"/>
+    <meta charset="UTF-8"/>
     <link rel="stylesheet" href="proj.css" type="text/css"/>
-	<title>Search result</title>
-    
+    <title>Search result</title>
 </head>
     
 <body>
         
-	<!-- Parent wrapper div -->
-	
- <div class="wrapper">
-        
+    <!-- Parent wrapper div -->
+    <div class="wrapper">
      
-   <!-- header wrapper div -->
+		<!-- header wrapper div -->
         <div class ="header">
-       
         <h1>This is the header</h1>
-		
-		
         </div>
-		
-         <?php include("menu.txt");?>
-		 
-		 <?php include("searchform.txt");?>
-		
-      <div class= "content">
-	 <?php
-	if(!isset($_GET['part']) || empty($_GET['part'])){
-		die("<p>No PartID found!</p>");// Denna rad skapar ogiltig html BTW
-	}
-	else
-	{
-	$part = $_GET['part'];
-	$prefix = "http://www.itn.liu.se/~stegu76/img.bricklink.com/PL/";
-	$prefix_colors = "http://www.itn.liu.se/~stegu76/img.bricklink.com/P/";
-	$connection = mysqli_connect("mysql.itn.liu.se","lego","","lego");
-            if (!$connection){
-                die("No connection to the lego database could be established.");
-            }
-			
-            $result = mysqli_query($connection, "SELECT DISTINCT parts.PartID, parts.partname, images.ItemTypeID, images.ItemID, images.has_largegif, images.has_largejpg FROM parts, images WHERE parts.PartID = '$part' AND images.ItemID=parts.PartID AND images.ItemTypeID='P'");
-			$result_colors = mysqli_query($connection, "SELECT DISTINCT inventory.ItemID, colors.ColorID, colors.Colorname, images.ItemID, images.has_gif, images.has_jpg FROM inventory, colors, images WHERE 
-			inventory.ItemID='$part' AND inventory.ColorID=colors.ColorID AND inventory.ItemID=images.ItemID LIMIT 10");
-			if($result->num_rows == 0){
-				$result = mysqli_query($connection, "SELECT DISTINCT PartID, partname FROM parts WHERE PartID = '$part'");
-			}
-            print("<table>\n<tr>");
-	        print("<th>Image</th> <th>PartID</th> <th>Partname</th> ");
-            print("</tr>\n");
-            while($row = mysqli_fetch_array($result))
-			{
-            $PartID = $row['PartID'];
-			$Partname = $row['partname'];
-			$Imagesource = $prefix . $PartID;
-			if($row['has_largejpg'])
-			{
-				$Imagesource .= ".jpg";
-				$Imageexists = true;
-			}
-			else if($row['has_largegif'])
-			{
-				$Imagesource .= ".gif";
-				$Imageexists = true;
-			}
-			else{
-				$Imageexists = false;
-			}
-			print("<tr>");
-			if ($Imageexists)
-				print("<td> <img src=\"$Imagesource\"></td>");
-			else
-				print("<td>No image avaliable!</td>");
-			print("<td>$PartID</td>");
-			print("<td>$Partname</td>");
-			print("</tr>");
-			}
-			print("</table>");
-	
-	
-        print("
-        </div>
-		
-		<div class=\"contentParent\">
-		
-		<div class=\"leftCol\">
-		<p> related parts </p>
-		</div>");
-		
-		print("<div class=\"leftCol\">");
-		print("<table>\n<tr>");
-		print("<th>Image</th> <th>Partname</th> <th>Colorname</th>");
-		print("</tr>");
-		while($row_colors = mysqli_fetch_array($result_colors))
-		{
-			$Colorname = $row_colors['Colorname'];
-			$ColorID = $row_colors['ColorID'];
-			$Imagesource_colors = $prefix_colors . "/" . $ColorID . "/" . $part;
-			print("<tr>");
-			if($row_colors['has_gif'])
-			{
-				print("<td><img src=\"$Imagesource_colors".".gif"."\" onerror=\"this.onerror=null;this.src='$Imagesource_colors".".jpg"."'\">");
-			}
-			else if($row_colors['has_jpg'])
-			{
-				print("<td><img src=\"$Imagesource_colors".".jpg"."\" onerror=\"this.onerror=null;this.src='$Imagesource_colors".".gif"."'\">");
-			}
-			else
-			{
-				print("<td> No image available! </td>");
-			}
-			print("<td>$Partname</td>");
-			print("<td>$Colorname</td>");
-			print("</tr>");
-		}
-		print("</table>");
-		print("</div>");
-		print("</div>");
-	}
-    ?>
+        
+         <?php
+include("menu.txt");
+?>
+        
+         <?php
+include("searchform.txt");
+?>
+       
+		<div class= "content">
+     <?php
+if (!isset($_GET['part']) || empty($_GET['part'])) {
+    die("<p>No PartID found!</p>"); // Denna rad skapar ogiltig html BTW
+} else {
+    $part          = $_GET['part'];
+    $prefix        = "http://www.itn.liu.se/~stegu76/img.bricklink.com/PL/";
+    $prefix_colors = "http://www.itn.liu.se/~stegu76/img.bricklink.com/P/";
+    $connection    = mysqli_connect("mysql.itn.liu.se", "lego", "", "lego");
+    if (!$connection) {
+        die("No connection to the lego database could be established.");
+    }
     
-
+    $result        = mysqli_query($connection, "SELECT DISTINCT parts.PartID, parts.partname, images.ItemTypeID, images.ItemID, images.has_largegif, images.has_largejpg FROM parts, images WHERE parts.PartID = '$part' AND images.ItemID=parts.PartID AND images.ItemTypeID='P'");
+    $result_colors = mysqli_query($connection, "SELECT DISTINCT inventory.ItemID, colors.ColorID, colors.Colorname, images.ItemID, images.has_gif, images.has_jpg FROM inventory, colors, images WHERE inventory.ItemID='$part' AND inventory.ColorID=colors.ColorID AND inventory.ItemID=images.ItemID LIMIT 10");
+    if ($result->num_rows == 0) {
+        $result = mysqli_query($connection, "SELECT DISTINCT PartID, partname FROM parts WHERE PartID = '$part'");
+    }
+    print("<table>\n<tr>");
+    print("<th>Image</th> <th>PartID</th> <th>Partname</th> ");
+    print("</tr>\n");
+    while ($row = mysqli_fetch_array($result)) {
+        $PartID      = $row['PartID'];
+        $Partname    = $row['partname'];
+        $Imagesource = $prefix . $PartID;
+        if ($row['has_largejpg']) {
+            $Imagesource .= ".jpg";
+            $Imageexists = true;
+        } else if ($row['has_largegif']) {
+            $Imagesource .= ".gif";
+            $Imageexists = true;
+        } else {
+            $Imageexists = false;
+        }
+        print("<tr>");
+        if ($Imageexists)
+            print("<td> <img src=\"$Imagesource\"></td>");
+        else
+            print("<td>No image avaliable!</td>");
+        print("<td>$PartID</td> <td>$Partname</td>");
+        print("</tr>");
+    }
+    print("</table>");
+    
+    
+    print("</div>");
+    print("<div class='contentParent'>");
+    print("<div class='leftCol'>");
+    print("<p>related parts</p>");
+    print("</div>");
+    print("<div class='leftCol'>");
+    print("<table>\n<tr>");
+    print("<th>Image</th> <th>Partname</th> <th>Colorname</th>");
+    print("</tr>");
+    while ($row_colors = mysqli_fetch_array($result_colors)) {
+        $Colorname          = $row_colors['Colorname'];
+        $ColorID            = $row_colors['ColorID'];
+        $Imagesource_colors = $prefix_colors . "/" . $ColorID . "/" . $part;
+        print("<tr>");
+        if ($row_colors['has_gif']) {
+            print("<td><img src='$Imagesource_colors.gif' onerror='this.onerror=null;this.src=\"$Imagesource_colors.jpg\"'>");
+        } else if ($row_colors['has_jpg']) {
+            print("<td><img src='$Imagesource_colors.jpg' onerror='this.onerror=null;this.src=\"$Imagesource_colors.gif\"'>");
+        } else {
+            print("<td> No image available! </td>");
+        }
+        print("<td>$Partname</td>");
+        print("<td><a href='searchresult_colors.php?ItemID=" . $part . "&ColorID=" . $ColorID . "'> $Colorname </a> </td>");
+        print("<td>$ColorID</td>");
+        print("</tr>");
+    }
+    print("</table>");
+    print("</div>");
+    print("</div>");
+}
+?>
+   
     <div class = "footer">
         <p>This is the footer</p>
-        
-        <!-- Footer content div closing tag  -->
+    <!-- Footer content div closing tag  -->
     </div>
-            
-<!-- wrapper content div closing tag  -->
- </div>
-    
+    <!-- wrapper content div closing tag  -->
+    </div> 
 </body>
-
 
 </html>
