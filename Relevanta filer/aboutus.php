f087d27f0bd42d0dca41c9d4c565ceebd4bf9630
Rubenbromee <45816070+Rubenbromee@users.noreply.github.com>
2019-01-07 13:34:14 +0100

    Add files via upload

---
+++ Relevanta filer/aboutus.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+
+<html>
+
+    
+<head>
+    
+     <meta charset="UTF-8"/>
+    <link rel="stylesheet" href="proj.css" type="text/css"/>
+	<title>About Us</title>
+		<link href="https://fonts.googleapis.com/css?family=Bree+Serif" rel="stylesheet">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+</head>
+    
+<body>
+        
+ <div class="wrapper">
+        
+     
+    
+        <div class ="header">
+       
+        <h1>This is the header</h1>
+		
+		
+		
+        </div>
+		
+	
+        		<nav>
+
+<ul>
+<li><a href="Homepage_V2.php">Home</a></li>
+<li><a href="howtosearch.php">How To Search</a></li>
+<li><a href="aboutus.php">About Us</a></li>
+</ul>
+
+</nav>
+
+		
+		
+		 
+      <div class= "content">
+        
+            <h1>About Us</h1>
+			<p>We are a group of students studying the course Electronic Publishing. The course required us to make a website with a search function that is linked to a database. In our case, the database used is a Lego-database that shows information about any existing piece.</p>
+		</div>	
+		
+		 
+	
+            <!-- contetn content div closing tag  -->
+        
+    
+    
+
+    <div class = "footer">
+        <p>This is the footer</p>
+		<?php include("footer.txt");?>
+        
+        <!-- Footer content div closing tag  -->
+    </div>
+            
+<!-- wrapper content div closing tag  -->
+ </div>
+    
+</body>
+
+
+</html>
---
+++ Relevanta filer/howtosearch.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+
+<html>
+
+    
+<head>
+    
+     <meta charset="UTF-8"/>
+    <link rel="stylesheet" href="proj.css" type="text/css"/>
+	<title>How to Search</title>
+		<link href="https://fonts.googleapis.com/css?family=Bree+Serif" rel="stylesheet">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+</head>
+    
+<body>
+        
+ <div class="wrapper">
+        
+     
+    
+        <div class ="header">
+       
+        <h1>This is the header</h1>
+		
+		
+		
+        </div>
+		
+	
+         		<nav>
+
+<ul>
+<li><a href="Homepage_V2.php">Home</a></li>
+<li><a href="howtosearch.php">How To Search</a></li>
+<li><a href="aboutus.php">About Us</a></li>
+</ul>
+
+</nav>
+		
+		 
+		
+		 
+      <div class= "content">
+        
+            <h1>hellohello how to serach??</h1>
+			<p>This database is dedicated to search for any piece of Lego in existence. It is possible to search for any piece and then see which set it belongs to, and also every variation of it.</p>
+            <!-- contetn content div closing tag  -->
+        </div>
+    
+    
+
+    <div class = "footer">
+        <p>This is the footer</p>
+        	<?php include("footer.txt");?>
+        <!-- Footer content div closing tag  -->
+    </div>
+            
+<!-- wrapper content div closing tag  -->
+ </div>
+    
+</body>
+
+
+</html>
