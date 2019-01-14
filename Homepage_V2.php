a7670bfb9001ca0f77f068102384b051367f78a5
Rubenbromee <45816070+Rubenbromee@users.noreply.github.com>
2019-01-14 13:35:54 +0100

    Fixd search showing invalid parts + cleanup

--- Homepage_V2.php
+++ Homepage_V2.php
@@ -45,110 +45,92 @@
         <!-- Sökresultat efter string management -->
         
         <?php
-		if(isset($_GET['searchkey']) && $_GET['searchkey'] != NULL)
-		{
-$searchkey = $_GET['searchkey'];
-$pagenumber = $_GET['pagenumber'];
-$offset = ($pagenumber-1) * 15;
-$previous_page = $pagenumber - 1;
-$next_page = $pagenumber + 1;
-trim($searchkey);
-$searchkey = htmlentities($searchkey);
-$searchkeyarray = str_split($searchkey);
-for ($i = 0; $i < count($searchkeyarray); $i++) {
-    if ($searchkeyarray[$i] == 'x') {
-        if (is_numeric($searchkeyarray[$i + 1])) {
-            array_splice($searchkeyarray, $i + 1, 0, ' ');
-        }
-        if (is_numeric($searchkeyarray[$i - 1])) {
-            array_splice($searchkeyarray, $i, 0, ' ');
-            $i++;
-        }
-    }
-}
-$searchkey = "";
-for ($i = 0; $i < count($searchkeyarray); $i++) {
-    $searchkey .= $searchkeyarray[$i];
-}
+if(isset($_GET['searchkey']) && $_GET['searchkey'] != NULL) {
+	$searchkey = $_GET['searchkey'];
+	$pagenumber = $_GET['pagenumber'];
+	$offset = ($pagenumber-1) * 15;
+	$previous_page = $pagenumber - 1;
+	$next_page = $pagenumber + 1;
+	$prefix        = "http://www.itn.liu.se/~stegu76/img.bricklink.com/PL/";
+	$prefix_colors = "http://www.itn.liu.se/~stegu76/img.bricklink.com/P/";
+	trim($searchkey);
+	$searchkey = htmlentities($searchkey);
+	$searchkeyarray = str_split($searchkey);
+	for ($i = 0; $i < count($searchkeyarray); $i++) {
+		if ($searchkeyarray[$i] == 'x') {
+			if (is_numeric($searchkeyarray[$i + 1])) {
+				array_splice($searchkeyarray, $i + 1, 0, ' ');
+			}
+			if (is_numeric($searchkeyarray[$i - 1])) {
+				array_splice($searchkeyarray, $i, 0, ' ');
+				$i++;
+			}
+		}
+	}
+	$searchkey = "";
+	for ($i = 0; $i < count($searchkeyarray); $i++) {
+		$searchkey .= $searchkeyarray[$i];
+	}
 ?>
       
         
         <div class="content">
 
             <?php
-$connection = mysqli_connect("mysql.itn.liu.se", "lego", "", "lego");
-if (!$connection) {
-    die("No connection to the lego database could be established.");
-}
-$searchkey = mysqli_real_escape_string($connection, $searchkey);
-$result = mysqli_query($connection, "SELECT DISTINCT * FROM parts WHERE partname LIKE '%$searchkey%' OR PartID LIKE '%$searchkey%' ORDER BY length(CatID), CatID, partname ASC LIMIT 100");
- $amount_of_results = 0;
- while(mysqli_fetch_array($result))
- {
-	 $amount_of_results ++;
- }
+	$connection = mysqli_connect("mysql.itn.liu.se", "lego", "", "lego");
+	if (!$connection) {
+		die("No connection to the lego database could be established.");
+	}
+	$searchkey = mysqli_real_escape_string($connection, $searchkey);
+	$result = mysqli_query($connection, "SELECT DISTINCT parts.PartID, parts.CatID, parts.Partname, inventory.ItemID FROM parts, inventory WHERE (parts.partname LIKE '%$searchkey%' OR parts.PartID LIKE '%$searchkey%') AND parts.PartID=inventory.ItemID ORDER BY length(CatID), CatID, partname ASC");
+	$amount_of_results = 0;
+	while(mysqli_fetch_array($result)) {
+		$amount_of_results ++;
+	}
  
- $amount_of_resultpages = ceil(($amount_of_results/15));
+	if($amount_of_results != 0) {
+		$amount_of_resultpages = ceil(($amount_of_results/15));
 
-$visible_result = mysqli_query($connection, "SELECT DISTINCT * FROM parts WHERE partname LIKE '%$searchkey%' OR PartID LIKE '%$searchkey%' ORDER BY length(CatID), CatID, partname ASC LIMIT 15 OFFSET $offset"); 
-
-echo"$searchkey";
-print("<table>\n<tr>");
-print("<th>PartID</th> <th>Partname</th>");
-print("</tr>\n");
-while ($row = mysqli_fetch_array($visible_result)) {
-    $PartID   = $row['PartID'];
-    $Partname = $row['Partname'];
-    print("<tr>");
-    print("<td>$PartID</td>");
-    print("<td><a href=\"Searchresult_parts.php?PartID=" . $PartID . "\">$Partname</a></td>");
-    print("</tr>");
-}
-print("</table>");
-if($pagenumber != 1 && $pagenumber != $amount_of_resultpages)
-{
-echo" 
-<form action='Homepage_V2.php' method='get'>\n
-<button type='submit' name='pagenumber' value='$previous_page'> Previous page </button>
-<input type='hidden' name='searchkey' value='$searchkey'>
-";
-echo"$pagenumber";
-echo"/";
-echo"$amount_of_resultpages";
-echo
-"
-<form action='Homepage_V2.php' method='get'>\n
-<button type='submit' name='pagenumber' value='$next_page'>Next page</button>
-<input type='hidden' name='searchkey' value='$searchkey'>
-";
-
-}
-else if($pagenumber == $amount_of_resultpages)
-{
-	echo" 
-<form action='Homepage_V2.php' method='get'>\n
-<button type='submit' name='pagenumber' value='$previous_page'> Previous page </button>
-<input type='hidden' name='searchkey' value='$searchkey'>
-";
-echo"$pagenumber";
-echo"/";
-echo"$amount_of_resultpages";
-}
-else
-{
-echo"$pagenumber";
-echo"/";
-echo"$amount_of_resultpages";
-echo
-"
-<form action='Homepage_V2.php' method='get'>\n
-<button type='submit' name='pagenumber' value='$next_page'>Next page</button>
-<input type='hidden' name='searchkey' value='$searchkey'>
-";
-}
-
+		$visible_result = mysqli_query($connection, "SELECT DISTINCT parts.PartID, parts.CatID, parts.Partname, inventory.ItemID FROM parts, inventory WHERE (parts.partname LIKE '%$searchkey%' OR parts.PartID LIKE '%$searchkey%') AND parts.PartID=inventory.ItemID ORDER BY length(CatID), CatID, partname ASC LIMIT 15 OFFSET $offset");
+		echo"<table>\n<tr>
+<th>Image </th> <th>PartID</th> <th>Partname</th>
+</tr>\n";
+		while ($row = mysqli_fetch_array($visible_result)) {
+			$PartID   = $row['PartID'];
+			$Partname = $row['Partname'];
+			$Imagesource = $prefix . $PartID;
+			echo"<tr>
+			<td><img src='$Imagesource.gif' onerror='this.onerror=null;this.src=\"$Imagesource.jpg\"' alt='No image avaliable!'> </td>
+			<td>$PartID</td>
+			<td><a href=\"Searchresult_parts.php?PartID=" . $PartID . "\">$Partname</a></td>
+			</tr>";
 		}
-
+		print("</table>");
+		if($pagenumber != 1 && $pagenumber != $amount_of_resultpages) {
+			echo "<form action='Homepage_V2.php' method='get'>\n
+			<button type='submit' name='pagenumber' value='$previous_page'> Previous page </button>
+			<input type='hidden' name='searchkey' value='$searchkey'>";
+			echo "$pagenumber/$amount_of_resultpages";
+			echo "<form action='Homepage_V2.php' method='get'>\n
+			<button type='submit' name='pagenumber' value='$next_page'>Next page</button>
+			<input type='hidden' name='searchkey' value='$searchkey'>";
+		} else if($pagenumber == $amount_of_resultpages) {
+			echo "<form action='Homepage_V2.php' method='get'>\n
+			<button type='submit' name='pagenumber' value='$previous_page'> Previous page </button>
+			<input type='hidden' name='searchkey' value='$searchkey'>";
+			echo"$pagenumber/$amount_of_resultpages";
+		}
+		else {
+			echo "$pagenumber/$amount_of_resultpages";
+			echo "<form action='Homepage_V2.php' method='get'>\n
+			<button type='submit' name='pagenumber' value='$next_page'>Next page</button>
+			<input type='hidden' name='searchkey' value='$searchkey'>";
+		}
+	} else {
+		print(" <p> Sorry, no results were found for \"$searchkey\". Try checking for misspellings and try again. <br>
+		If you're unsure how to search. Click the \"How to search\" button at the top of your screen. </p> ");
+	}
+}
 ?>
 
 
