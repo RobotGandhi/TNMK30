30d82b358dd53f994fbac880af8cd7abe8f8149e
RobotGandhi <99viktor.vc@gmail.com>
2019-01-18 14:03:33 +0100

    AboutUs cleaned

--- aboutus.php
+++ aboutus.php
@@ -1,60 +1,40 @@
 <!DOCTYPE html>
 
-<html>
-
-    
-<head>
+<html>   
+    <head>
         <meta charset="UTF-8"/>
         <title>About Us | Lego Finder</title>
         <meta name="description" content="A site where you can find your lego parts and see what set they're in!"/>
         <meta name="author" content="Viktor Carlsson, Uma Eriksson, Ruben Bromee, Jessie Chow, Alma Fernvik"/>
         <link rel="stylesheet" href="style.css" type="text/css"/>
-		<link href="https://fonts.googleapis.com/css?family=Bree+Serif" rel="stylesheet">
-		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href="https://fonts.googleapis.com/css?family=Bree+Serif" rel="stylesheet">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
     </head>
+    
     <body>
-	<div class="wrapper">
-		<a class="icon" href="Homepage_V2.php">
-    <div class ="header">
-       
-		
+        <a class="icon" href="Homepage_V2.php">
+        <div class ="header">
         <h1>Lego finder</h1>
-		</div>
-		
-		</a>
-		
-		<nav>
-
-<ul>
-<li><a href="Homepage_V2.php">Home</a></li>
-<li><a href="aboutus.php">About Us</a></li>
-</ul>
-
-</nav>
-		 
-      <div class= "content">
+        </div>
+        </a>
         
-            <h1>About us</h1>
-			<p>We are a group of five students studying the course Electronic Publishing at the University of LinkÃ¶ping, Sweden. 
-			Our names are Viktor Carlsson, Uma Eriksson, Ruben Bromée, Jessie Chow and Alma Fernvik.</p>
-			
-			<p>The course required us to make a website with a search function that is linked to a database. 
-			In our case, the search function is linked to a Lego-database where you can search for a lego part and see what sets it's included in.</p>
-		</div>	
-		
-		 
-	
-            <!-- content content div closing tag  -->
-        
-    
-    
-
-<?php include("footer.txt");?>
+        <nav>
+        <ul>
+        <li><a href="Homepage_V2.php">Home</a></li>
+        <li><a href="aboutus.php">About Us</a></li>
+        </ul>
+        </nav>
+         
+        <div class= "content">
+        <h1>About us</h1>
+        <p>We are a group of five students studying the course Electronic Publishing at the University of LinkÃ¶ping, Sweden. 
+        Our names are Viktor Carlsson, Uma Eriksson, Ruben Bromée, Jessie Chow and Alma Fernvik.</p>
             
-<!-- wrapper content div closing tag  -->
- </div>
+        <p>The course required us to make a website with a search function that is linked to a database. 
+        In our case, the search function is linked to a Lego-database where you can search for a lego part and see what sets it's included in.</p>
+        </div>
+        
+<?php include("footer.txt"); ?>
     
-</body>
-
-
+    </body>
 </html>
