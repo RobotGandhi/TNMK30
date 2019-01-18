e92c1550057cbbbb0b7a58bd4cef38079b13fce1
UmaEriksson <45813476+UmaEriksson@users.noreply.github.com>
2019-01-18 14:31:11 +0100

    jef

--- aboutus.php
+++ aboutus.php
@@ -1,4 +1,4 @@
-<!DOCTYPE html>
+﻿<!DOCTYPE html>
 
 <html>
 
@@ -7,7 +7,7 @@
         <meta charset="UTF-8"/>
         <title>About Us | Lego Finder</title>
         <meta name="description" content="A site where you can find your lego parts and see what set they're in!"/>
-        <meta name="author" content="Viktor Carlsson, Uma Eriksson, Ruben Bromee, Jessie Chow, Alma Fernvik"/>
+        <meta name="author" content="Viktor Carlsson, Uma Eriksson, Ruben Bromée, Jessie Chow, Alma Fernvik"/>
         <link rel="stylesheet" href="style.css" type="text/css"/>
 		<link href="https://fonts.googleapis.com/css?family=Bree+Serif" rel="stylesheet">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -36,7 +36,7 @@
         
             <h1>About us</h1>
 			<p>We are a group of five students studying the course Electronic Publishing at the University of Linköping, Sweden. 
-			Our names are Viktor Carlsson, Uma Eriksson, Ruben Brom�e, Jessie Chow and Alma Fernvik.</p>
+			Our names are Viktor Carlsson, Uma Eriksson, Ruben Bromée, Jessie Chow and Alma Fernvik.</p>
 			
 			<p>The course required us to make a website with a search function that is linked to a database. 
 			In our case, the search function is linked to a Lego-database where you can search for a lego part and see what sets it's included in.</p>
